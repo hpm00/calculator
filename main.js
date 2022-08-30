be2545530cf7421a8dbf1c36767b97ed801fbf53
husna <hpm25@georgetown.edu>
2022-08-30 08:52:38 -0700

    add simple math functions

---
+++ main.js
@@ -0,0 +1,37 @@
+
+// populate the display
+function display() {
+
+};
+display();
+
+// 
+function operator() {
+
+};
+operator();
+
+
+
+
+
+// simple math functions
+function addition(num1, num2) {
+    return num1 + num2;
+};
+addition();
+
+function subtraction(num1, num2) {
+    return num1 - num2
+};
+subtraction();
+
+function division(num1, num2) {
+    return num1 / num2
+}
+division(num1,num2);
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+multiply();
