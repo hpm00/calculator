4a56b2f5d288d1f5db0c8984af212729340c83b7
husna <hpm25@georgetown.edu>
2022-08-30 09:17:05 -0700

    declare num1 and num2 variables

--- main.js
+++ main.js
@@ -5,31 +5,20 @@ function display() {
 };
 display();
 
-
-
-
-
-
-
-
-
-
-
+let num1 = '';
+let num2 = '';
 // call this function based on what the operator is
 function operator(operator, num1, num2) {
 
     switch(operator) {
         case '+':
             return addition(num1, num2);
-            break;
         case '-': 
             return subtraction(num1, num2);
-            break;
         case 'x':
             return multiply(num1, num2);
-            break;
         case '/':
-            if (num2 === '0') {
+            if (num2 === 0) {
                 return 'null'
             }
             else {
@@ -53,7 +42,7 @@ subtraction();
 function division(num1, num2) {
     return num1 / num2
 }
-division(num1,num2);
+division(num1, num2);
 
 function multiply(num1, num2) {
     return num1 * num2
