f569fb1d95c11181812da462a2711ab2e36a4586
husna <hpm25@georgetown.edu>
2022-08-30 09:06:51 -0700

    call this function based on operator parameter

--- main.js
+++ main.js
@@ -5,15 +5,39 @@ function display() {
 };
 display();
 
-// 
-function operator() {
 
+
+
+
+
+
+
+
+
+
+// call this function based on what the operator is
+function operator(operator, num1, num2) {
+
+    switch(operator) {
+        case '+':
+            return addition(num1, num2);
+            break;
+        case '-': 
+            return subtraction(num1, num2);
+            break;
+        case 'x':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            if (num2 === '0') {
+                return 'null'
+            }
+            else {
+                return division(num1, num2);
+            };
+    };   
 };
 operator();
-
-
-
-
 
 // simple math functions
 function addition(num1, num2) {
